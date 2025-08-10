4c4ef0753da906c4af121e5892e260b22cca121c
Pratik Das <pratikdasmartingarrix@gmail.com>
2025-08-11 00:49:49 +0530

    Button Update

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,29 @@
-const Button = () => {
+const Button = ({ text, onClickHandler, styletype = "primary" }) => {
   return (
-    <button> 
+    <button 
+    onClick={onClickHandler}
+    className={`${getButtonStyling(styletype)} text-white py-2 px-4 border-none rounded cursor-pointer`}
+    >
+      {text}
     </button>
   );
 };
+
+function getButtonStyling(styletype) {
+  switch (styletype) {
+    case "primary":
+      return "bg-blue-500 text-white";
+    case "secondary":
+      return "bg-gray-500 text-white";
+    case "error":
+      return "bg-red-500 text-white";
+    case "success":
+      return "bg-green-500 text-white";
+    case "warning":
+      return "bg-yellow-500 text-white";
+    default:
+      return "bg-blue-500 text-white";
+  }
+}
 
 export default Button;
