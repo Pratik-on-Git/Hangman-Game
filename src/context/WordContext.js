3bbf9aa8811d4ee2eea148333a28d267fa85eb43
Pratik Das <pratikdasmartingarrix@gmail.com>
2025-09-06 15:49:56 +0530

    WordContext

---
+++ src/context/WordContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const WordContext = createContext(); // Create a context object for managing word-related state
+
+export default WordContext;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
+import WordContext from './context/WordContext.js';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <BrowserRouter>
-      <App />
+      <WordContext.Provider>
+        <App />
+      </WordContext.Provider>
     </BrowserRouter>
-  </StrictMode>,
 )
